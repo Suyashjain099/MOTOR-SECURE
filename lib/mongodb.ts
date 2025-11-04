c02ba1cf28bf778e0aa4f2a6c3347d13c30201a4
Suyashjain099 <suyashjaindamoh@gmail.com>
2025-11-04 13:42:18 +0530

    Add fallback MongoDB URI for deployment

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,6 +1,6 @@
 import mongoose from 'mongoose'
 
-const MONGODB_URI = process.env.MONGODB_URI!
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://suyash:1529@cluster0.h7v0tbt.mongodb.net/smart-bike-lock?retryWrites=true&w=majority'
 
 if (!MONGODB_URI) {
   throw new Error('Please define the MONGODB_URI environment variable inside .env.local')
